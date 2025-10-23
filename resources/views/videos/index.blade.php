c45cd3ef9d72e3cfe0b3319bb89e915da14a7775
SunnieHere <ramasyamsi545@gmail.com>
2025-10-23 18:58:21 +0700

    Update index.blade.php

--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -13,9 +13,13 @@
             @foreach ($videos as $video)
                 <a href="{{ route('videos.show', $video) }}" class="bg-[#121212] rounded-lg shadow hover:shadow-lg transition overflow-hidden block hover:scale-105 hover:bg-[#080808]">
                     @if ($video->thumbnail_path)
-                        <img src="{{ asset('storage/' . $video->thumbnail_path) }}" alt="{{ $video->title }}" class="w-full h-48 object-cover">
+                        <img src="{{ $video->thumbnail_path }}" alt="{{ $video->title }}" class="w-full h-48 object-cover">
                     @else
-                        <img src="{{ asset('images/default-thumbnail.jpg') }}" alt="No Thumbnail" class="w-full h-48 object-cover">
+                        <div class="w-full h-48 bg-gray-700 flex items-center justify-center">
+                            <svg class="w-16 h-16 text-gray-500" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"/>
+                            </svg>
+                        </div>
                     @endif
                     <div class="p-4">
                         <h3 class="font-semibold text-lg text-white">{{ $video->title }}</h3>
